6ba5ff382723273ca1288d8a13e17d61e0e85617
MahmoudSaber1 <136622762+MahmoudSaber2@users.noreply.github.com>
2024-02-17 10:29:11 +0200

    Create an image page & api

--- schemas/index.ts
+++ schemas/index.ts
@@ -5,3 +5,11 @@ export const conversationSchema = z.object({
         message: "Prompt is required.",
     }),
 });
+
+export const imageSchema = z.object({
+    prompt: z.string().min(1, {
+        message: "Photo prompt is required",
+    }),
+    amount: z.string().min(1),
+    resolution: z.string().min(1),
+});
